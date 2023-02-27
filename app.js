50b0f81cee7f4ea9a6571acdca5c360024dee6e2
Gurleen Kaur <gurleenkaur2607@gmail.com>
2023-02-27 19:39:06 +0530

    updated the redirect url

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ passport.deserializeUser(function(id, done){
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://secrets-ke86.onrender.com/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     //console.log(profile);
